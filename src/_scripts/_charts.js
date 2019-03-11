74dca1e21f2e3ee1f9452d718c658eac79b0b402
etokec <etoke.c@gmail.com>
2019-03-11 16:42:49 -0700

    add interactivity

--- src/_scripts/_charts.js
+++ src/_scripts/_charts.js
@@ -30,7 +30,7 @@ function createChart(el, fieldname){
 
   var xAxis = d3.axisBottom(xScale)
                 .tickValues([2000,2005,2010,2015,2017]);
-                
+
   var yAxis = d3.axisLeft(yScale)
                 .tickSize(-chartWidth)
                 .ticks(4)
@@ -43,6 +43,9 @@ function createChart(el, fieldname){
       .attr('class','y axis')
       .call(yAxis)
 
+  var tooltip = svg.append('text')
+                   .attr('class','chart-tooltip');
+
   svg.selectAll('.bar')
      .data(annualTotals)
      .enter()
@@ -52,6 +55,19 @@ function createChart(el, fieldname){
      .attr('y', d => yScale(d[fieldname]))
      .attr('width', xScale.bandwidth())
      .attr('height', d => chartHeight - yScale(d[fieldname]))
+     .on('mouseenter', function(d){
+          // centers the text above each bar
+          var x = xScale(d.year) + xScale.bandwidth() / 2 ;
+          // the -5 bumps up the text a bit so it's not directly over the bar
+          var y = yScale(d[fieldname]) - 5;
+          d3.select(this).classed('highlight',true);
+          tooltip.text(d[fieldname])
+                 .attr('transform', `translate(${x},${y})`);
+        })
+     .on('mouseleave', function(d) {
+          d3.select(this).classed('highlight', false);
+          tooltip.text('');
+     }) ;
 
 }
 
